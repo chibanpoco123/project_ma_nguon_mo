ba47ae95ee748787aee81d9cbec018a44dfc186e
chibanpco123 <hoang26022005sc@gmail.com>
2025-11-16 17:23:58 +0700

    checkout api giỏ hàng tiến hàng với momo

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Home';
 import Login from '../components/page/login';
+import Checkout from '../components/page/checkout';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/checkout" element={<Checkout />} />
     </Routes>
   );
 }
