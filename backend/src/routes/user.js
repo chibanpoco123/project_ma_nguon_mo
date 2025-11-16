24bdb5b532408ca09ab7aac19fc448249f4f3130
VanKhangPham <phamvankhang182004@gmail.com>
2025-11-16 22:18:49 +0700

    ...

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -16,19 +16,19 @@ router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 
-// 🆕 👉 ADMIN tạo người dùng ()
+// ADMIN tạo người dùng ()
 router.post("/create", verifyToken, isAdmin, createUser);
 
-// 👉 Danh sách user (chỉ admin)
+// Danh sách user (chỉ admin)
 router.get("/", verifyToken, isAdmin, getAllUsers);
 
-// 👉 Lấy thông tin chi tiết user
+//  Lấy thông tin chi tiết user
 router.get("/:id", verifyToken, getUserById);
 
-// 👉 Cập nhật thông tin (user hoặc admin)
+// Cập nhật thông tin (user hoặc admin)
 router.put("/:id", verifyToken, updateUser);
 
-// 👉 Xóa người dùng
+// Xóa người dùng
 router.delete("/:id", verifyToken, deleteUser);
 
 export default router;
